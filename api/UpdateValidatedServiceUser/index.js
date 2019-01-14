b75005b9b04e5f8d63fe710d24e86817cce78c0a
Elody <elody@bfansports.com>
2019-01-14 15:56:00 +0100

    lambda to upadte th validated column for a service_user

---
+++ api/UpdateValidatedServiceUser/index.js
@@ -0,0 +1,35 @@
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+    host: "jay.czsbtwrelfgz.eu-west-1.rds.amazonaws.com",
+    user: "jay",
+    password: "jaypassword",
+    database: "jay",
+});
+
+// console.log(connection);
+exports.handler =  (event, context, callback) => {
+    //Update validated
+    let sql = 'UPDATE service_user SET validated = ? WHERE id = ?';
+    let data = [event.validated, event._id];
+    console.log(event);
+    console.log(sql, data);
+    //prevent timeout from waiting event loop
+    context.callbackWaitsForEmptyEventLoop = false;
+    pool.getConnection(function(err, connection) {
+        // Use the connection
+        connection.query(sql, data, function (error, results, fields) {
+            // And done with the connection.
+            connection.release();
+            // Handle error after the release.
+            if (error) {
+                console.log("Failure to update validated column to service_user");
+                callback(error);
+            }
+            else {
+                callback(null,results);
+            }
+        });
+    });
+};
+
